06c02d81e5e943637639457cf1f5fa7c4ef591b3
JJLibra <2565474095@qq.com>
2024-10-05 21:36:37 +0800

    🦄 refactor:

--- generator/image.py
+++ generator/image.py
@@ -1,6 +1,5 @@
-import numpy as np
 from PIL import Image
-
+import numpy as np
 
 class PokemonImage:
     def __init__(self, image: Image.Image):
@@ -16,7 +15,7 @@ class PokemonImage:
         image_array = np.array(self.image)
 
         alpha_channel = image_array[:, :, 3]
-        # first and last non transparent pixels taken as start and end
+        # first and last non-transparent pixels taken as start and end
         # points for crop along both height and width axes
         non_zero_x_values, non_zero_y_values = alpha_channel.nonzero()
         top, left = np.min(non_zero_x_values), np.min(non_zero_y_values)
@@ -29,3 +28,12 @@ class PokemonImage:
         if self.image.mode != "RGBA":
             rgba_image = self.image.convert("RGBA")
             self._image = rgba_image
+
+    def resize_to_max_height(self, max_height: int = 20) -> None:
+        """Resize the image to have a maximum height of max_height, maintaining the aspect ratio."""
+        current_width, current_height = self._image.size
+        if current_height > max_height:
+            # Calculate the new width maintaining the aspect ratio
+            new_width = int((max_height / current_height) * current_width)
+            # Resize the image
+            self._image = self._image.resize((new_width, max_height), Image.LANCZOS)
